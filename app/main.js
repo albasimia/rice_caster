1fe3d3886cef71aaaf9fc8e05537d81b4f831b3f
albasimia <albasimia888@gmail.com>
2021-06-26 02:30:55 +0900

    ツイキャス画面を非表示に（暫定）

--- app/main.js
+++ app/main.js
@@ -17,17 +17,32 @@ const server = require('http').createServer(function (request, response) {
 const io = require('socket.io')(server);
 
 
-
+const winWidth = 500;
+const winHeight = 500;
 // electron初期化
-class App {  constructor(electron) {
+class App {
+  constructor(electron) {
     this.app = electron.app;
+    this.mainView;
     this.commentView;
     this.app.on('window-all-closed', function () {
       if (process.platform !== 'darwin') {
         this.app.quit();
       };
     });
-    this.app.on('ready', function () {
+    this.app.addListener('ready', function () {
+      this.mainView = new electron.BrowserWindow({
+        width: winWidth,
+        height: winHeight,
+        webPreferences: {
+          nodeIntegration: true,
+          contextIsolation: false,
+        }
+      });
+      this.mainView.loadURL('file://' + __dirname + '/mainView/index.html')
+
+
+      // commentView
       const size = electron.screen.getPrimaryDisplay().size
       this.commentView = new electron.BrowserWindow({
         width: size.width,
@@ -47,57 +62,81 @@ class App {  constructor(electron) {
     // this.commentView.webContents.openDevTools()
   }
 }
-new App(electron);
 
+const commentApp = new App(electron);
 const main = async () => {
   await pie.initialize(electron.app);
   const browser = await pie.connect(electron.app, puppeteer);
 
-  const window = new electron.BrowserWindow();
-  const url = "https://twitcasting.tv/black_ape/windowcomment?embedded=1&auth_user_id=black_ape&auth_key=mu89hz0sbz";
-  await window.loadURL(url);
 
-  const page = await pie.getPage(browser, window);
-  // window.destroy();
+  let view = new electron.BrowserView({
+    webPreferences: {
+      nodeIntegration: false
+    }
+  })
+  // commentApp.app.addListener('ready', async function () {
+    // commentApp.mainView.setBrowserView(view);
+    // view.setBounds({
+    //   x: winWidth / 2,
+    //   y: 0,
+    //   width: winWidth / 2,
+    //   height: winHeight
+    // })
+    const url = "https://twitcasting.tv/black_ape/windowcomment?embedded=1&auth_user_id=black_ape&auth_key=mu89hz0sbz";
+    // await view.webContents.loadURL(url)
+    const window = new electron.BrowserWindow({
+        width: 0,
+        height: 0,
+        transparent: true,
+        frame: false,
+        resizable: false,
+    });
+    await window.loadURL(url);
 
-  let adddom = await page.evaluate(() => {
-    const target = document.querySelector('.tw-comment-list-view__scroller div')
-    const observer = new MutationObserver(records => {
-      const comment = records[0].addedNodes[0];
-      const nameEle = comment.querySelector('.tw-comment-item-name')
-      const commentEle = comment.querySelector('.tw-comment-item-comment')
-      const commentUserId = comment.querySelector('.tw-comment-item-screen-id')
-      const itemImage = comment.querySelector('.tw-comment-item-attachment img')
-      // const itemComment = comment.querySelector('.tw-comment-item-comment')
-      const resObj = {
-        userId: commentUserId.innerText,
-        name: nameEle.innerText,
-        dataType: comment.getAttribute('data-type'),
-        comment: String(commentEle.innerHTML.replace(/&/g, "&amp;")
-          .replace(/"/g, "&quot;")
-          .replace(/</g, "&lt;")
-          .replace(/>/g, "&gt;")),
-        date: commentEle.getAttribute('title'),
-        itemImage: itemImage ? itemImage.getAttribute('src') : ''
-        // itemImage: String(itemImage.innerHTML.replace(/&/g, "&amp;")
-        // .replace(/"/g, "&quot;")
-        // .replace(/</g, "&lt;")
-        // .replace(/>/g, "&gt;")),
-      }
-      console.log(JSON.stringify(resObj))
-      // console.log(comment.querySelector('.tw-comment-item'))
-      // console.log(comment)
+    // const page = await pie.getPage(browser, view);
+    const page = await pie.getPage(browser, window);
+    // window.destroy();
+
+    let adddom = await page.evaluate(() => {
+      const target = document.querySelector('.tw-comment-list-view__scroller div')
+      const observer = new MutationObserver(records => {
+        const comment = records[0].addedNodes[0];
+        const nameEle = comment.querySelector('.tw-comment-item-name')
+        const commentEle = comment.querySelector('.tw-comment-item-comment')
+        const commentUserId = comment.querySelector('.tw-comment-item-screen-id')
+        const itemImage = comment.querySelector('.tw-comment-item-attachment img')
+        // const itemComment = comment.querySelector('.tw-comment-item-comment')
+        const resObj = {
+          userId: commentUserId.innerText,
+          name: nameEle.innerText,
+          dataType: comment.getAttribute('data-type'),
+          comment: String(commentEle.innerHTML.replace(/&/g, "&amp;")
+            .replace(/"/g, "&quot;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")),
+          date: commentEle.getAttribute('title'),
+          itemImage: itemImage ? itemImage.getAttribute('src') : ''
+          // itemImage: String(itemImage.innerHTML.replace(/&/g, "&amp;")
+          // .replace(/"/g, "&quot;")
+          // .replace(/</g, "&lt;")
+          // .replace(/>/g, "&gt;")),
+        }
+        console.log(JSON.stringify(resObj))
+        // console.log(comment.querySelector('.tw-comment-item'))
+        // console.log(comment)
+      })
+      observer.observe(target, {
+        childList: true
+      })
     })
-    observer.observe(target, {
-      childList: true
+    page.on('console', async (msg) => {
+      // console.log(msg._text)
+      // console.log(mainWindow.webContents)
+      // mainWindow.webContents.send('ping', 'whoooooooh!')
+      io.emit('comment', msg._text);
     })
-  })
-  page.on('console', async (msg) => {
-    // console.log(msg._text)
-    // console.log(mainWindow.webContents)
-    // mainWindow.webContents.send('ping', 'whoooooooh!')
-    io.emit('comment', msg._text);
-  })
+  // })
+
 };
 
 main();
