78e539b1d9fddd55d7d3fa7dd52ab323db33fa18
albasimia <albasimia888@gmail.com>
2021-07-09 00:23:16 +0900

    logを残すように修正。

--- app/main.js
+++ app/main.js
@@ -3,6 +3,7 @@ const path = require('path');
 const pie = require("puppeteer-in-electron");
 const puppeteer = require("puppeteer-core");
 const ElectronPreferences = require('electron-preferences');
+const log = require('electron-log');
 const default_config = require('./default_config.json');
 const config_template = require('./config_template.json');
 
@@ -38,9 +39,12 @@ class App {
     this.app.on('window-all-closed', () => {
       if (process.platform !== 'darwin') {
         this.quit();
+        log.info('quite')
       };
     });
     this.app.addListener('ready', function () {
+      log.info('app ready')
+      try{
       this.mainView = new electron.BrowserWindow({
         width: winWidth,
         height: winHeight,
@@ -55,7 +59,12 @@ class App {
 
       this.mainView.on('closed', () => {
         this.quit();
+        log.info('quite')
+        
       });
+    }catch(error){
+      log.error(error)
+    }
     });
   }
   openOverlay() {
@@ -70,8 +79,14 @@ class App {
       alwaysOnTop: true,
       useContentSize: true,
     });
+    log.info('commentView load : http://localhost:' + preferences.value('basic.port'))
     this.commentView.loadURL('http://localhost:' + preferences.value('basic.port'));
     this.commentView.setIgnoreMouseEvents(true);
+
+    this.commentView.on('closed', () => {
+      log.info('commentView closed')
+      mainView.webContents.send('overlayClosed');
+    });
     commentView = this.commentView;
   }
   closeOverlay() {
@@ -95,6 +110,7 @@ let browser;
 
 // 関数実行用のリスナー
 ipcMain.on('eval', function (e, arg) {
+  log.info('evaluate function : ' + arg)
   eval(arg + '()');
 });
 
@@ -111,85 +127,98 @@ const closeOverlay = () => {
 let twicasView;
 let pingSender;
 const connect = async () => {
+  try {
+    // サーバー初期化
+    server = require('http').createServer(function (request, response) {
+      request.addListener('end', function () {
+        file.serve(request, response);
+      }).resume();
+    }).listen(preferences.value('basic.port'));
+    io = require('socket.io')(server);
+    log.info('created server')
 
-  // サーバー初期化
-  server = require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-      file.serve(request, response);
-    }).resume();
-  }).listen(preferences.value('basic.port'));
-  io = require('socket.io')(server);
+    // 設定変更を監視
+    // preferences.on('save', (preferences) => {
+    //   io.emit('changePreferenses', preferences);
+    // });
 
-  // 設定変更を監視
-  // preferences.on('save', (preferences) => {
-  //   io.emit('changePreferenses', preferences);
-  // });
-
-  const url = preferences.value('basic.comment_url');
-  twicasView = new electron.BrowserWindow({
-    width: 0,
-    height: 0,
-    transparent: true,
-    frame: false,
-    resizable: false,
-  });
-  await twicasView.loadURL(url);
-
-  // twicasのコメントを取得
-  const page = await pie.getPage(browser, twicasView);
-
-  let adddom = await page.evaluate(() => {
-    const target = document.querySelector('.tw-comment-list-view__scroller div')
-    const observer = new MutationObserver(records => {
-      const comment = records[0].addedNodes[0];
-      const nameEle = comment.querySelector('.tw-comment-item-name')
-      const commentEle = comment.querySelector('.tw-comment-item-comment')
-      const commentUserId = comment.querySelector('.tw-comment-item-screen-id')
-      const itemImage = comment.querySelector('.tw-comment-item-attachment img')
-      const resObj = {
-        userId: commentUserId.innerText,
-        name: nameEle.innerText,
-        dataType: comment.getAttribute('data-type'),
-        comment: String(commentEle.innerHTML.replace(/&/g, "&amp;")
-          .replace(/"/g, "&quot;")
-          .replace(/</g, "&lt;")
-          .replace(/>/g, "&gt;")),
-        date: commentEle.getAttribute('title'),
-        itemImage: itemImage ? itemImage.getAttribute('src') : ''
-        // itemImage: String(itemImage.innerHTML.replace(/&/g, "&amp;")
-        // .replace(/"/g, "&quot;")
-        // .replace(/</g, "&lt;")
-        // .replace(/>/g, "&gt;")),
-      }
-      console.log(JSON.stringify(resObj))
-    })
-    observer.observe(target, {
-      childList: true
-    })
-  })
-  page.on('console', async (msg) => {
-    io.emit('comment', msg._text);
-  })
-
-  io.on('connection', (socket)=>{
-    io.emit('changePreferenses', preferences.value());
-    socket.on("disconnect", (reason) => {
-      console.log({"disconect": reason})
+    const url = preferences.value('basic.comment_url');
+    twicasView = new electron.BrowserWindow({
+      width: 0,
+      height: 0,
+      transparent: true,
+      frame: false,
+      resizable: false,
     });
+    log.info('twicasView load : '+ url)
+    await twicasView.loadURL(url);
 
-    // 接続を維持
-    pingSender = setInterval( function() {
-      io.emit( "ping" );
-    }, 30000);
-    // socket.on('pong', ()=>{
-    //   console.log('pong');
-    // })
-  })
-  
-  mainView.webContents.send("connected");
+    // twicasのコメントを取得
+    const page = await pie.getPage(browser, twicasView);
+
+    let adddom = await page.evaluate(() => {
+      const target = document.querySelector('.tw-comment-list-view__scroller div')
+      const observer = new MutationObserver(records => {
+        const comment = records[0].addedNodes[0];
+        const nameEle = comment.querySelector('.tw-comment-item-name')
+        const commentEle = comment.querySelector('.tw-comment-item-comment')
+        const commentUserId = comment.querySelector('.tw-comment-item-screen-id')
+        const itemImage = comment.querySelector('.tw-comment-item-attachment img')
+        const resObj = {
+          userId: commentUserId.innerText,
+          name: nameEle.innerText,
+          dataType: comment.getAttribute('data-type'),
+          comment: String(commentEle.innerHTML.replace(/&/g, "&amp;")
+            .replace(/"/g, "&quot;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")),
+          date: commentEle.getAttribute('title'),
+          itemImage: itemImage ? itemImage.getAttribute('src') : ''
+          // itemImage: String(itemImage.innerHTML.replace(/&/g, "&amp;")
+          // .replace(/"/g, "&quot;")
+          // .replace(/</g, "&lt;")
+          // .replace(/>/g, "&gt;")),
+        }
+        console.log(JSON.stringify(resObj))
+      })
+      observer.observe(target, {
+        childList: true
+      })
+    })
+    page.on('console', async (msg) => {
+      log.info('comment json : ' + msg._text)
+      io.emit('comment', msg._text);
+    })
+
+    io.on('connection', (socket) => {
+      log.info('twicasView connection')
+      io.emit('changePreferenses', preferences.value());
+      socket.on("disconnect", (reason) => {
+        log.info("disconect : " + reason)
+      });
+
+      // 接続を維持
+      pingSender = setInterval(function () {
+        io.emit("ping");
+      }, 30000);
+      // socket.on('pong', ()=>{
+      //   console.log('pong');
+      // })
+    })
+
+    mainView.webContents.send("connected");
+  } catch (error) {
+    log.error(error);
+    io.close();
+    io = null;
+    server.close();
+    server = null;
+    io = null;
+  }
 };
 
 const disconnect = () => {
+  log.info('Intentional disconnection')
   twicasView.destroy();
   if (commentView) {
     commentView.close();
